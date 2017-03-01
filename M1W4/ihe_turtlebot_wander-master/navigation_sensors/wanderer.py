ea2d464c4b56b9beb2a7418d82b7bc662f43a081
turtlebot <turtlebot@turtlebot.(none)>
2017-03-01 18:29:07 -0500

    This code is probably not the way to go

--- M1W4/ihe_turtlebot_wander-master/navigation_sensors/wanderer.py
+++ M1W4/ihe_turtlebot_wander-master/navigation_sensors/wanderer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 import rospy
+import math
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
+from kobuki_msgs.msg import BumperEvent
+from kobuki_msgs.msg import WheelDropEvent
+from math import radians
+import time
 
 class Scan_msg:
 
@@ -14,55 +19,29 @@ class Scan_msg:
         3 dictionaries are to keep track of the movement and log messages.'''
         self.pub = rospy.Publisher('/cmd_vel_mux/input/navi',Twist)
         self.msg = Twist()
+        self.average1 = 0
+        self.average2 = 0
+        self.average3 = 0
+        self.average4 = 0
+        self.average5 = 0
+
         self.sect_1 = 0
         self.sect_2 = 0
         self.sect_3 = 0
         self.sect_4 = 0
         self.sect_5 = 0
-        self.ang = {00000:0,
-                    00001:-1.2,
-                    00010:-1.2,
-                    00011:-1.2,
-                    00100:1.5,
-                    00101:1.0,
-                    00110:1.0,
-                    00111:1.2,
-                    01000:,
-                    01001:,
-                    01010:,
-                    01011:,
-                    01100:,
-                    01101:,
-                    01110:,
-                    01111:,
-                    10000:0,
-                    10001:-1.2,
-                    10010:-1.2,
-                    10011:-1.2,
-                    10100:1.5,
-                    10101:1.0,
-                    10110:1.0,
-                    10111:1.2,
-                    11000:,
-                    11001:,
-                    11010:,
-                    11011:,
-                    11100:,
-                    11101:,
-                    11110:,
-                    11111:,
-                    } # Should 111 be -1.2
-        self.fwd = {0:.25,1:0,10:0,11:0,100:0.1,101:0,110:0,111:0}
-        self.dbgmsg = {0:'Move forward',1:'Veer right',10:'Veer right',11:'Veer right',100:'Veer left',101:'Veer left',110:'Veer left',111:'Veer right'}
+        self.ang = {1:.6, 2:.6, 3:0, 4:-.6, 5:-.6}
+        self.fwd = {1:.05, 2:.2, 3:.3, 4:.2, 5:.05}
+        self.dbgmsg = {1:'Turn Left', 2:'Veer Left', 3:'Move Straight', 4:'Veer Right', 5:'Turn Right'}
 
-
-    def reset_sect(self):
+                
+    def reset_averages(self):
         '''Resets the below variables before each new scan message is read'''
-        self.sect_1 = 0
-        self.sect_2 = 0
-        self.sect_3 = 0
-        self.sect_4 = 0
-        self.sect_5 = 0
+        self.average1 = 0
+        self.average2 = 0
+        self.average3 = 0
+        self.average4 = 0
+        self.average5 = 0
 
     def sort(self, laserscan):
         '''Goes through 'ranges' array in laserscan message and determines 
@@ -72,31 +51,75 @@ class Scan_msg:
 
         Parameter laserscan is a laserscan message.'''
         entries = len(laserscan.ranges)
-        for entry in range(0,entries):
-            if 0.4 < laserscan.ranges[entry] < 0.75:
-                self.sect_1 = 1 if (0 < entry < entries/5) else 0 
-                self.sect_2 = 1 if (1*entries/5 < entry < 2*entries/5) else 0
-                self.sect_3 = 1 if (2*entries/5 < entry < 3*entries/5) else 0
-                self.sect_4 = 1 if (3*entries/5 < entry < 4*entires/5) else 0
-                self.sect_5 = 1 if (4*entries/5 < entry < entries) else 0
-        rospy.loginfo("sort complete,sect_1: " + str(self.sect_1) + " sect_2: " + str(self.sect_2) + " sect_3: " + str(self.sect_3) + " sect_4: " + str(self.sect_4) + " sect_5: " + str(self.sect_5))
+        maxValue = laserscan.range_max
+        minValue = laserscan.range_min
+        rospy.loginfo(str(maxValue) + "    " + str(minValue))
 
-    def movement(self, sect1, sect2, sect3, sect4, sect5):
+        totalEntries1 = 0
+        totalEntries2 = 0
+        totalEntries3 = 0
+        totalEntries4 = 0
+        totalEntries5 = 0
+        toSubtract1 = 0
+        toSubtract2 = 0
+        toSubtract3 = 0
+        toSubtract4 = 0
+        toSubtract5 = 0
+        for entry in range(0, entries/5):
+            if not (math.isnan(laserscan.ranges[entry])):
+                totalEntries1 += laserscan.ranges[entry]
+            else:
+                toSubtract1 += 1 
+        self.average1 = totalEntries1/(entries/5 - toSubtract1 + 1)
+
+        for entry in range(entries/5, 2*entries/5):
+            if not (math.isnan(laserscan.ranges[entry])):
+                totalEntries2 += laserscan.ranges[entry]
+            else:
+                toSubtract2 += 1     
+        self.average2 = totalEntries2/(entries/5 - toSubtract2 + 1)
+
+        for entry in range(2*entries/5, 3*entries/5):
+            if not (math.isnan(laserscan.ranges[entry])):
+                totalEntries3 += laserscan.ranges[entry]
+            else:
+                toSubtract3 += 1  
+        self.average3 = totalEntries3/(entries/5 - toSubtract3 + 1)
+
+        for entry in range(3*entries/5, 4*entries/5):
+            if not (math.isnan(laserscan.ranges[entry])):
+                totalEntries4 += laserscan.ranges[entry]
+            else:
+                toSubtract4 += 1 
+        self.average4 = totalEntries4/(entries/5 - toSubtract4 + 1)
+
+        for entry in range(4*entries/5, entries):
+            if not (math.isnan(laserscan.ranges[entry])):
+                totalEntries5 += laserscan.ranges[entry]
+            else:
+                toSubtract5 += 1 
+        self.average5 = totalEntries5/(entries/5 - toSubtract5 + 1)
+
+        rospy.loginfo('End of finding averages')
+
+    def movement(self):
         '''Uses the information known about the obstacles to move robot.
 
         Parameters are class variables and are used to assign a value to
         variable sect and then  set the appropriate angular and linear 
         velocities, and log messages.
         These are published and the sect variables are reset.'''
-        sect = int(str(self.sect_1) + str(self.sect_2) + str(self.sect_3) + str(self.sect_4) + str(self.sect_5))
-        rospy.loginfo("Sect = " + str(sect)) 
-        
-        self.msg.angular.z = self.ang[sect]
-        self.msg.linear.x = self.fwd[sect]
-        rospy.loginfo(self.dbgmsg[sect])
+        averages = [self.average1,self.average2,self.average3,self.average4,self.average5]
+        #for average in averages:
+        #    rospy.loginfo("1: " + str(average))
+        rospy.loginfo(averages)
+        maxSector = averages.index(max(averages)) + 1
+        self.msg.angular.z = self.ang[maxSector]
+        self.msg.linear.x = self.fwd[maxSector]
+        rospy.loginfo(self.dbgmsg[maxSector])
         self.pub.publish(self.msg)
 
-        self.reset_sect()
+        self.reset_averages()
  
     def for_callback(self,laserscan):
         '''Passes laserscan onto function sort which gives the sect 
@@ -105,7 +128,7 @@ class Scan_msg:
 
         Parameter laserscan is received from callback function.'''
         self.sort(laserscan)
-        self.movement(self.sect_1, self.sect_2, self.sect_3, self.sect_4, self.sect_5)
+        self.movement()
     
 
 def call_back(scanmsg):
@@ -119,8 +142,42 @@ def listener():
     function.'''
     rospy.init_node('navigation_sensors')
     rospy.loginfo("Subscriber Starting")
+    pub = rospy.Publisher('/cmd_vel_mux/input/navi',Twist)
+
+    #rospy.Subscriber("/mobile_base/events/wheel_drop", WheelDropEvent, self.WheelDropEventCallback)
+    r = rospy.Rate(5)
+
+    rightTurn = Twist()
+    rightTurn.angular.z = -radians(60)
+    leftTurn = Twist()
+    leftTurn.angular.z = radians(60)
+    centerBumper = Twist()
+    centerBumper.linear.x = -.2
     sub = rospy.Subscriber('/scan', LaserScan, call_back)
+    subBumper = rospy.Subscriber("/mobile_base/events/bumper", BumperEvent, self.BumperEventCallback)
     rospy.spin()
+
+def BumperEventCallback(self, data):
+    rospy.loginfo("In Bumper Callback")
+    if (data.state == BumperEvent.PRESSED):
+        if (data.bumper == BumperEvent.LEFT):
+            for i in range(0, 10):
+                pub.publish(self.rightTurn)
+                self.r.sleep()
+            #rospy.sleep(1.)
+            #time.sleep(1)
+        elif (data.bumper == BumperEvent.RIGHT):
+            for i in range(0, 10):
+                self.pub.publish(self.leftTurn)
+                self.r.sleep()
+            #rospy.sleep(1.)
+            #time.sleep(1)
+        else:
+            for i in range(0, 10):
+                self.pub.publish(self.centerBumper)
+                self.r.sleep()
+            #rospy.sleep(1.)
+            #time.sleep(1)
 
 if __name__ == "__main__":
     '''A Scan_msg class object called sub_obj is created and listener
